3d7871ecf66ac982b45f16f7053b615e1ce392b4
shritejmayekar <shritejmayekar@gmail.com>
2017-10-05 14:16:40 +0530

    modified

--- com/bridgelabz/util/utility/Util.java
+++ com/bridgelabz/util/utility/Util.java
@@ -174,6 +174,30 @@ public class Util {
 			return true;
 		return false;
 	}
+	public static void dayOfWeek(int month,int day,int year) {
+		int nyear=0,nday=0,nmonth=0;
+		String strMonth[]={"","January","February","March","April"
+		,"May","June","July","August","September","October","November","December"};
+		String strDay[]={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+		nyear=year-(14-month)/12;
+		int temp=nyear+nyear/4-nyear/100+nyear/400;
+		nmonth=month+12*((14-month)/12)-2;
+		nday=(day+temp+31*nmonth/12)%7;
+		
+		System.out.println(strMonth[month]+" "+day+" "+strDay[nday]);
+	
+	}
+	public static double temperatureConversionF(double temperature) {
+		return (temperature*9)/5+32;
+	}
+	public static double temperatureConversionC(double temperature) {
+		return (temperature-32)*5/9;
+	}
+	public static void main(String args[]) {
+	dayOfWeek(10,5,2017);
+	System.out.println(temperatureConversionF(36));
+	System.out.println(temperatureConversionC(98));
+	}
 	
 }
 
